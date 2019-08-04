655d0591bfe428672a7ffcc05aa481f0b88432d4
Martin Broerse <broerse@martinic.com>
2019-08-04 09:28:54 +0200

    Create new productUpdate object for updating products

--- index.js
+++ index.js
@@ -19,14 +19,36 @@ async function main(params) {
   try {
     const response1 = await fetch( exchangeratesApi + '/latest?base=' + baseCurrency, {method:'GET', headers: headers });
     const exchangerates = await response1.json();
-    console.log(exchangerates);
+    console.log('exchangerates: ', exchangerates);
     const response2 = await fetch(fastspringApi + '/products', {method:'GET', headers: authHheaders });
     const products = await response2.json();
+
     for (let productId of products.products) {
-      console.log(productId);
+      console.log('ID: ', productId);
       const response3 = await fetch(fastspringApi + '/products/' + productId, {method:'GET', headers: authHheaders });
       const product = await response3.json();
       basePrice = product.products[0].pricing.price[baseCurrency];
+
+      let productUpdate = {
+        "products": [
+          {
+            "pricing": {
+            }
+          }
+        ]
+      };
+
+      Object.assign(productUpdate.products[0].pricing, product.products[0].pricing);
+      productUpdate.products[0].product = product.products[0].product;
+      productUpdate.products[0].display = product.products[0].display;
+      delete productUpdate.products[0].pricing.cancellation;
+      delete productUpdate.products[0].pricing.dateLimitsEnabled;
+      if (!productUpdate.products[0].pricing.quantityDiscounts) {
+        productUpdate.products[0].pricing.quantityDiscounts = {};
+      }
+      if (!productUpdate.products[0].pricing.quantityDiscounts[productUpdate.products[0].pricing.quantityDefault]) {
+        productUpdate.products[0].pricing.quantityDiscounts[productUpdate.products[0].pricing.quantityDefault] = 0;
+      }
       for (let currency of currencies) {
         if (exchangerates.rates[currency]) {
           let value =  (basePrice * exchangerates.rates[currency]).toFixed(2);
@@ -38,30 +60,15 @@ async function main(params) {
             }
             value = roundValue + '.' + centsBase;
           }
-          console.log(currency, value);
-          product.products[0].pricing.price[currency] = value;
+          productUpdate.products[0].pricing.price[currency] = value;
         }
       }
-      if (!product.products[0].pricing.quantityDiscounts) {
-        product.products[0].pricing.quantityDiscounts = {};
-      }
-      if (!product.products[0].pricing.quantityDiscounts[product.products[0].pricing.quantityDefault]) {
-        product.products[0].pricing.quantityDiscounts[product.products[0].pricing.quantityDefault] = 0;
-      }
-      delete product.products[0].action;
-      delete product.products[0].result;
-      delete product.products[0].parent;
-      delete product.products[0].fulfillments;
 
-      delete product.products[0].pricing.cancellation;
-      delete product.products[0].pricing.dateLimitsEnabled;
+      console.log('products: ', JSON.stringify(productUpdate, null, 2));
 
-      console.log(JSON.stringify(product, null, 2));
-  //    console.log(product.products[0]);
-
-      const response4 = await fetch(fastspringApi + '/products/' + productId, {method:'POST', headers: authHheaders, body: JSON.stringify(product)});
+      const response4 = await fetch(fastspringApi + '/products/' + productId, {method:'POST', headers: authHheaders, body: JSON.stringify(productUpdate)});
       const result = await response4.json();
-      console.log(result.products[0]);
+      console.log('result: ', result.products[0]);
     }
 
   } catch (error) {
